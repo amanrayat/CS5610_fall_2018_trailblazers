fb3def55846a9c2c1edcffbb260aa7b350451e32
amanrayat <amanrayat32@gmail.com>
2018-11-22 18:34:18 -0500

    Login page

---
+++ frontend/beerewery/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class Navbar extends React.Component{
+    // constructor(props){
+    //     super(props);
+    // }
+
+    render(){
+        return(
+            <h1>Navbar</h1>
+        )
+    }
+}
