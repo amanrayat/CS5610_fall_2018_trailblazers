570083517a4486f9bf7db1e408077c620c0766bb
amanrayat <amanrayat32@gmail.com>
2018-11-22 20:34:09 -0500

    register

---
+++ frontend/beerewery/src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default class Home extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <li>
+                    <ul>Login</ul>
+                    <ul>Register</ul>
+                </li>
+
+            </div>
+        )
+    }
+}
--- frontend/beerewery/src/index.js
+++ frontend/beerewery/src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Navbar from './components/Navbar'
 import Login from './components/Login/Login'
+import Register from './components/Register/Register'
 
 function App() {
     return (
-     <Login/>
+     <Register/>
     );
 }
 
