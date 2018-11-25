eb2a01c1ddafa44b1b4da8445be99bf7ef066889
Koosh Doctor <kooshdoctor@Kooshs-MacBook-Pro.local>
2018-11-25 18:14:22 -0500

    Implemented reloading of the same component with a new search query

--- frontend/beerewery/src/components/SearchResult/SearchResult.js
+++ frontend/beerewery/src/components/SearchResult/SearchResult.js
@@ -1,21 +1,83 @@
 import React from 'react';
 import queryString from 'query-string'
+import {Redirect, Router} from 'react-router-dom'
+import { withRouter } from 'react-router-dom';
+
 import Navbar from "../NavBar/Navbar";
 
-export default class SearchResult extends React.Component {
+class SearchResult extends React.Component {
 
     constructor(props){
         super(props);
+        let query = queryString.parse(this.props.location.search, { ignoreQueryPrefix: true }).q;
         this.state = {
-            searchQuery: queryString.parse(this.props.location.search, { ignoreQueryPrefix: true }).q
+            initialSearchQuery: query,
+            searchQuery: query,
+            beerActive: true,
+            breweryActive: false
         };
     }
 
     componentDidMount(){
-
     }
 
+    componentWillReceiveProps(newProps){
+        let query = queryString.parse(newProps.location.search, { ignoreQueryPrefix: true }).q;
+        this.setState({
+            initialSearchQuery: query,
+            searchQuery: query,
+            beerActive: true,
+            breweryActive: false
+        });
+        window.location.reload();
+    }
+
+    trackSearchTextChanges = (e) => {
+        this.setState({
+            searchQuery: e.target.value,
+        })
+    };
+
+    toggleBeer = () => {
+        if(!this.state.beerActive){
+            this.setState({
+                beerActive: true,
+                breweryActive: false,
+            })
+        }
+    };
+
+    toggleBrewery = () => {
+        if(!this.state.breweryActive){
+            this.setState({
+                beerActive: false,
+                breweryActive: true
+            })
+        }
+    };
+
+    sendSearchRequest = () => {
+        if(this.state.searchQuery.length !== 0 && this.state.searchQuery !== this.state.initialSearchQuery){
+            this.setState({
+                moveToSearchResult: true,
+            });
+        }
+    };
+
+    generateSearchRoute = () => {
+        let newPath = '/search-results?q=';
+        let queryStringSplit = this.state.searchQuery.split(" ");
+        var i;
+        for(i = 0; i < queryStringSplit.length; i++){
+            newPath += queryStringSplit[i] + '+'
+        }
+        return newPath.slice(0,-1);
+    };
+
     render(){
+        if(this.state.moveToSearchResult){
+            return <Redirect to={this.generateSearchRoute()} />
+        }
         return(
             <div className="h-100">
                 <Navbar/>
@@ -26,13 +88,13 @@ export default class SearchResult extends React.Component {
 
                     </div>
                     <div className="col-md-6 col-12 px-0">
-                        <div className="row bg-light">
+                        <div className="row bg-light border">
                             <div className="col-12">
                                 <div className="row my-4">
                                     <div className="col-10">
                                         <input
                                             value={this.state.searchQuery}
-                                            //onChange={this.trackSearchTextChanges}
+                                            onChange={this.trackSearchTextChanges}
                                             className="form-control"
                                             type="text"
                                             placeholder="Search for Beers or Breweries"
@@ -40,13 +102,27 @@ export default class SearchResult extends React.Component {
                                     </div>
                                     <div className="col-2">
                                         <button
-                                            //onClick={this.sendSearchRequest}
+                                            onClick={this.sendSearchRequest}
                                             type="button"
                                             className="btn btn-secondary">
                                             Search
                                         </button>
                                     </div>
                                 </div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div
+                                className={"col-6 py-2 text-center " + (this.state.beerActive? "bg-white": "bg-light border")}
+                                onClick={this.toggleBeer}
+                            >
+                                Beer
+                            </div>
+                            <div
+                                className={"col-6 py-2 text-center " + (this.state.breweryActive? "bg-white": "bg-light border")}
+                                onClick={this.toggleBrewery}
+                            >
+                                Brewery
                             </div>
                         </div>
                     </div>
@@ -58,3 +134,5 @@ export default class SearchResult extends React.Component {
         )
     }
 }
+
+export default withRouter(SearchResult)
