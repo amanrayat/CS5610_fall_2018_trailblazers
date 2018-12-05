3d95ebf8f634e1ce8794ff77edb35a84d5a48eb9
amanrayat <amanrayat32@gmail.com>
2018-12-05 00:26:31 -0500

    Added router

--- beerewery/src/index.js
+++ beerewery/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import AppContainer from "./containers/AppContainer";
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 function App() {
     return (
-     <AppContainer/>
+        <Router>
+            <Route path='/' component={AppContainer}/>
+        </Router>
     );
 }
 
