9067910a2caf983c1eca5c021e3462778f93e0b3
Koosh Doctor <kooshdoctor@Kooshs-MacBook-Pro.local>
2018-12-06 15:07:42 -0500

    Implemented admin

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -8,6 +8,7 @@ import NotFound from "../components/NotFound/NotFound";
 import AppliedRoute from "../components/AppliedRoute";
 import AuthenticatedRoute from "../components/AuthenticatedRoute";
 import UnauthenticatedRoute from "../components/UnauthenticatedRoute";
+import Admin from "../components/Admin/Admin";
 
 export default ({ childProps }) =>
     <Switch>
@@ -16,6 +17,7 @@ export default ({ childProps }) =>
         <UnauthenticatedRoute path="/register" exact component={Register} props={childProps} />
         <AuthenticatedRoute path="/profile" exact component={Profile} props={childProps} />
         <UnauthenticatedRoute path="/profile/:profileId" exact component={Profile} props={childProps} />
+        <AppliedRoute path = "/admin" exact component = {Admin} props = {childProps}/>
         {/*<AuthenticatedRoute path="/course/:courseId/edit" exact component={CourseEditor} props={childProps} />*/}
         { /* Finally, catch all unmatched routes */ }
         <Route component={NotFound} />
