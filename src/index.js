011d09af25fdd5bb2481312b2e5a19d90c449ef2
Koosh Doctor <kooshdoctor@Kooshs-MacBook-Pro.local>
2018-12-06 17:04:31 -0500

    Added a user

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
 import * as serviceWorker from './serviceWorker';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
